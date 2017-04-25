c9bba3918f7c8745294c7dafbe5c5fa14957e7d0
mpattrakorn <mark29162@gmail.com>
2017-04-26 01:03:22 +0700

    frontend updated

--- web/app/app.component.ts
+++ web/app/app.component.ts
@@ -2,7 +2,13 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  styleUrls: [
+    'app/app.component.css',
+    'app/css/style.css',
+    'app/css/animations.css',
+    'app/color/default.css',
+    'app/css/nivo-lightbox.css'
+  ]
 })
 export class AppComponent {
 }
